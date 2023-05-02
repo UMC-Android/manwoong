601b681184a5d44129816dd36ca4645b40e91460
manwoong <cheonwoong21@naver.com>
2023-05-02 14:15:44 +0900

    second

--- app/src/main/java/com/example/app4/MainActivity.kt
+++ app/src/main/java/com/example/app4/MainActivity.kt
@@ -6,12 +6,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import com.example.app4.databinding.ActivityMainBinding
+import android.app.AlertDialog
+
 
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var nextIntent: Intent
     private lateinit var binding: ActivityMainBinding
+    private var editTextContent: String = ""
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -21,40 +25,47 @@ class MainActivity : AppCompatActivity() {
         Log.d("lifecycle","onCreate")
 
         nextIntent = Intent(this,SecondActivity::class.java)
-        nextIntent.putExtra("email", "cheonwoong21@naver.com")   // Intent에 이메일 주소 넣기
-        nextIntent.putExtra("password", 1234)
 
         binding.btnnext.setOnClickListener { // 버튼 클릭시 할 행동
+            nextIntent.putExtra("editTextContent", binding.viewContent.text.toString())  // EditText의 내용을 Intent에 추가합니다.
             startActivity(nextIntent)  // 화면 전환하기
         }
+
     }
-
-
-
-    override fun onStart() {
-        super.onStart()
-        Log.d("lifecycle","onStart")
-    }
-
     override fun onResume() {
         super.onResume()
-        Log.d("lifecycle","onResume")
+
+        if (editTextContent.isNotEmpty()) {
+            binding.viewContent.setText(editTextContent)
+        }
+        // onPause에서 저장한 전역변수 내용으로 EditText 내용 설정
     }
 
-    override fun onPause(){
+    override fun onPause() {
         super.onPause()
-        Log.d("lifecycle","onPause")
+
+        editTextContent = binding.viewContent.text.toString()
+        // 현재까지 작성한 내용 전역변수에 저장
     }
 
-    override fun onStop() {
-        super.onStop()
-        Log.d("lifecycle","onStop")
+    override fun onRestart() {
+        super.onRestart()
+
+        val builder = AlertDialog.Builder(this)
+        builder.setMessage("계속 작성하시겠습니까?")
+        builder.setPositiveButton("네") { dialog, _ ->
+            dialog.dismiss()
+        }
+        builder.setNegativeButton("아니오") { dialog, _ ->
+            editTextContent = ""
+            binding.viewContent.text.clear()
+            dialog.dismiss()
+        }
+        builder.create().show()
+        // Dialog를 활용하여 다시 작성할지 묻는 창 띄우기
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.d("lifecycle","onDestroy")
-    }
+
 
 
 
--- app/src/main/java/com/example/app4/SecondActivity.kt
+++ app/src/main/java/com/example/app4/SecondActivity.kt
@@ -14,11 +14,9 @@ class SecondActivity : AppCompatActivity() {
         binding = ActivitySecondBinding.inflate(layoutInflater) // setContentView 대신에 뷰 바인딩 루트 뷰를 사용하여 액티비티 레이아웃을 설정합니다.
         setContentView(binding.root)
 
-        val id = intent.getStringExtra("email") // Intent에서 Key를 email로 가지고 있는 값 가져오기
-        val pw= intent.getIntExtra("password", 0) // Intent에서 Key를 password로 가지고 있는 값 가져오기
-        // 두번째 인자 값의 뜻은.. 값을 찾지 못했을땐 0을 대신 넣겠다는 뜻
+        val text = intent.getStringExtra("editTextContent") // Intent에서 Key를 email로 가지고 있는 값 가져오기
 
-        binding.email.text = id             // 텍스트뷰에 아이디 출력
-        binding.passwd.text = pw.toString()  // 텍스트뷰에 비밀번호 출력
+        binding.cont.text = text          // 텍스트뷰에 아이디 출력
+
     }
 }
