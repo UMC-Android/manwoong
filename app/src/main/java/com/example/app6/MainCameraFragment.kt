0c0ce946ce8f310057966a3deb1efdc7cfdf0614
manwoong <cheonwoong21@naver.com>
2023-05-11 03:05:14 +0900

    second

--- app/src/main/java/com/example/app6/MainCameraFragment.kt
+++ app/src/main/java/com/example/app6/MainCameraFragment.kt
@@ -1,52 +1,75 @@
 package com.example.app6
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
+import com.example.app6.databinding.FragmentMainCameraBinding
+import me.relex.circleindicator.CircleIndicator3
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [MainCameraFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class MainCameraFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentMainCameraBinding? = null
+    private val binding get() = _binding!!
+
+    private val numPage = 5
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main_camera, container, false)
+        _binding = FragmentMainCameraBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        val mPager = binding.viewpager
+        val mIndicator = binding.indicator
+
+        val pagerAdapter = MyAdapter(requireActivity(), numPage)
+        mPager.adapter = pagerAdapter
+
+        mIndicator.setViewPager(mPager)
+        mIndicator.createIndicators(numPage, 0)
+
+        mPager.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+
+        mPager.currentItem = 1000
+        mPager.offscreenPageLimit = 4
+
+        mPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageScrolled(
+                position: Int,
+                positionOffset: Float,
+                positionOffsetPixels: Int
+            ) {
+                super.onPageScrolled(position, positionOffset, positionOffsetPixels)
+                if (positionOffsetPixels == 0) {
+                    mPager.currentItem = position
+                }
+            }
+
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                mIndicator.animatePageSelected(position % numPage)
+            }
+        })
+
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MainCameraFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             MainCameraFragment().apply {
