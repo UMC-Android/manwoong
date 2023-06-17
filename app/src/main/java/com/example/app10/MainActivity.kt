5fffd27f1f0dafba7d3c499b96e904155589d61b
manwoong <cheonwoong21@naver.com>
2023-06-17 19:09:31 +0900

    initial

---
+++ app/src/main/java/com/example/app10/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.app10
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.lifecycleScope
+import com.example.app10.databinding.ActivityMainBinding
+import com.kakao.sdk.common.KakaoSdk
+import com.kakao.sdk.common.model.ClientError
+import com.kakao.sdk.common.model.ClientErrorCause
+import com.kakao.sdk.user.UserApiClient
+import kotlinx.coroutines.launch
+
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        KakaoSdk.init(this, getString(R.string.kakao_native_app_key))
+
+        binding.buttonKakaoLogin.setOnClickListener{
+            lifecycleScope.launch {
+                try {
+                    // 서비스 코드에서는 간단하게 로그인 요청하고 oAuthToken 을 받아올 수 있다.
+                    val oAuthToken = UserApiClient.loginWithKakao(this@MainActivity)
+                    Log.d("MainActivity", "beanbean > $oAuthToken")
+                } catch (error: Throwable) {
+                    if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
+                        Log.d("MainActivity", "사용자가 명시적으로 취소")
+                    } else {
+                        Log.e("MainActivity", "인증 에러 발생", error)
+                    }
+                }
+            }
+        }
+    }
+
+
+}
